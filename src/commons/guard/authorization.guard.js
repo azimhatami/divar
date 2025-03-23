1ba15e7c5aafd6d43bdb9dcb421305677468fb61
Azim Hatami <azimhatami.dev@gmail.com>
2025-03-24 00:17:00 +0330

    Implemented token reading and verification from browser cookies for authentication

---
+++ src/commons/guard/authorization.guard.js
@@ -0,0 +1,30 @@
+const createHttpError = require('http-errors');
+const authorizationMSG = require('../messages/auth.message');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const userModel = require('../../modules/user/user.model');
+
+
+const authorization = async (req, res, next) => {
+  try {
+    const token = req?.cookie?.access_token;
+    if (!token) {
+      throw new createHttpError.Unauthorized(authorizationMSG.Login)
+    }
+
+    const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    if (typeof data === 'object' && 'id' in data) {
+      const user = await userModel.findById(data.id, {accessToken: 0, otp: 0}).lean();
+      if (!user) throw new createHttpError.Unauthorized(authorizationMSG.NotFoundAccount)
+      req.user = user;
+      return next();
+    }
+    throw new createHttpError.Unauthorized(authorizationMSG.InvalidToken)
+  } catch (error) {
+    next(error)
+  }
+};
+
+
+module.exports = authorization;
---
+++ src/commons/messages/auth.message.js
@@ -0,0 +1,10 @@
+const authorizationMSG = Object.freeze({
+  Login: 'login on your account',
+  LoginAgain: 'pls login again',
+  Unauthorized: 'unauthorized please login on your account',
+  NotFoundAccount: 'not found account',
+  InvalidToken: 'token is invalid',
+});
+
+
+module.exports = authorizationMSG;
