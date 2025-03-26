1c455c4453c8553ddd1db5212c21106e58504fa7
Azim Hatami <azimhatami.dev@gmail.com>
2025-03-26 12:19:24 +0330

    Add user logout functionality

--- src/modules/auth/auth.messages.js
+++ src/modules/auth/auth.messages.js
@@ -4,7 +4,8 @@ const AuthMessages = {
   NotFound: 'User not found',
   OtpCodeNotExpired: 'OTP still active, Try later.',
   OtpCodeExpired: 'OTP code expired. Request a new one.',
-  OtpCodeIsIncorrect: 'Incorrect OTP code. Try again.'
+  OtpCodeIsIncorrect: 'Incorrect OTP code. Try again.',
+  Logout: 'logout successful',
 };
 
 module.exports = {
